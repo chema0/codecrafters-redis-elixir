a6ea9f174da066da2d938e19de2cfdca581998f5
chema <dev.josemariamm@gmail.com>
2024-02-24 21:33:53 +0100

    pass replication 4th stage

--- lib/replication.ex
+++ lib/replication.ex
@@ -1,10 +1,15 @@
 defmodule Replication do
+  @master_replid "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"
+  @master_repl_offset 0
+
   def get_replication_info() do
     config = Storage.get(:config) || %{}
 
     case Map.get(config, :replica_of) do
       nil ->
-        Builder.build_bulk_string("# Replication\nrole:master")
+        Builder.build_bulk_string(
+          "# Replication\nrole:master\nmaster_replid:#{@master_replid}\nmaster_repl_offset:#{@master_repl_offset}"
+        )
 
       _ ->
         Builder.build_bulk_string("# Replication \nrole:slave")
